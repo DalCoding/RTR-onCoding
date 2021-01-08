6edacc8c53aa11f4dfe70dcd84f3a397346910ff
blanc0548 <kimbr0548@gmail.com>
2021-01-08 15:11:39 +0900

    키 해시 코드 수정

--- app/src/main/java/com/example/rotory/MainPage.java
+++ app/src/main/java/com/example/rotory/MainPage.java
@@ -1,12 +1,21 @@
 package com.example.rotory;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainPage extends AppCompatActivity {
 
@@ -15,15 +24,15 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.big_map_page);
 
-        MapView mapView = new MapView(this);
+        /*MapView mapView = new MapView(this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.bigMapLayout);
-        mapViewContainer.addView(mapView);
+        mapViewContainer.addView(mapView);*/
 
 
 
-        // 디버그 키 해쉬 값 찾기 (카카오맵 API 연동 시 필요)
-        /*try {
+        // 디버그 키 해시 구하기 (카카오 맵 API 연동 시 필요), 맵 코드 주석 처리 후 실행! (애뮬에서 돌리면 실행 오류 날 수 있음)
+        try {
             PackageInfo info = getPackageManager().getPackageInfo("com.example.rotory", PackageManager.GET_SIGNATURES);
             for (Signature signature : info.signatures) {
                 MessageDigest md = MessageDigest.getInstance("SHA");
@@ -36,6 +45,6 @@ public class MainPage extends AppCompatActivity {
             e.printStackTrace();
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
