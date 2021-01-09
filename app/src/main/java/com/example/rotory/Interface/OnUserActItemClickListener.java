517cf5a6441e2120bab0e4fc251919ae60b209c5
uoo0708 <75959589+uoo0708@users.noreply.github.com>
2021-01-09 19:31:03 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/rotory/Interface/OnUserActItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.rotory.Interface;
+
+public interface OnUserActItemClickListener {
+    public void OnStarClicked();
+
+    public void OnLikeClicked();
+
+    public void OnFlagClicked();
+
+    public void OnLinkClicked();
+}
