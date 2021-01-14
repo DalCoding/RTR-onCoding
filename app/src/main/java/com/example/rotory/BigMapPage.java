490dfafa323d0416388964a70d77f674d2b95a78
blanc0548 <75959342+blanc0548@users.noreply.github.com>
2021-01-14 09:40:23 +0900

    fragment_line.xml 수정

--- app/src/main/java/com/example/rotory/BigMapPage.java
+++ app/src/main/java/com/example/rotory/BigMapPage.java
@@ -30,7 +30,6 @@ import net.daum.mf.map.api.MapView;
 public class BigMapPage extends AppCompatActivity {
     private static final String TAG = "BigMapPage";
 
-    LinearLayout bigMapBtnLayout;
 
     @Override
     protected void onCreate(@NonNull Bundle savedInstanceState) {
@@ -289,83 +288,4 @@ public class BigMapPage extends AppCompatActivity {
 
 
 
-}*/
-
-
-/*
-import android.os.Bundle;
->>>>>>> 9e88d5ab87ffd04bf9157d143ea44986e3fb0210
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-<<<<<<< HEAD
-=======
-import android.widget.ImageView;
->>>>>>> 9e88d5ab87ffd04bf9157d143ea44986e3fb0210
-import android.widget.LinearLayout;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import net.daum.mf.map.api.MapView;
-
-public class BigMapPage extends Fragment {
-
-    public static BigMapPage newInstance() {
-        return new BigMapPage();
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.big_map_page, container, false);
-        initUI(rootView);
-        return rootView;
-    }
-
-    private void initUI(ViewGroup rootView) {
-
-        //FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        MapView bigMapView = new MapView(getContext());
-        ViewGroup mapView = (ViewGroup) rootView.findViewById(R.id.bigMapLayout);
-        mapView.addView(bigMapView);
-
-        MapView bigMapView = new MapView(getContext());
-        ViewGroup mapView = (ViewGroup) rootView.findViewById(R.id.bigMapLayout);
-        mapView.addView(bigMapView);
-
-        Button thisBigMapBtn = rootView.findViewById(R.id.thisBigMapBtn);
-        ImageButton bigMapMyLocationBtn = rootView.findViewById(R.id.bigMapMyLocationBtn);
-
-        //initUI(rootView);
-        return rootView;
-    }
-
-    *//*private void initUI(ViewGroup rootView) {
-
-        //FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        *//**//*MapView bigMapView = new MapView(getActivity());
-        ViewGroup mapView = (ViewGroup) rootView.findViewById(R.id.bigMapLayout);
-        mapView.addView(bigMapView);*//**//*
-
-        Button thisBigMapBtn = rootView.findViewById(R.id.thisBigMapBtn);
-        ImageButton bigMapMyLocationBtn = rootView.findViewById(R.id.bigMapMyLocationBtn);
-
-
-
-    }
-}
-
-
-    }
-}
-    }
 }*/
