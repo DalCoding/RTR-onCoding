eb09d64acfb5b4c98e87f6abb763959a11ed3c46
suhwan20 <lamboccult@naver.com>
2021-01-14 11:33:12 +0900

    지도(길작성페이지) 업데이트

--- app/src/main/java/com/example/rotory/WriteMapPage.java
+++ app/src/main/java/com/example/rotory/WriteMapPage.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -22,7 +23,10 @@ import androidx.core.app.ActivityCompat;
 
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPolyline;
 import net.daum.mf.map.api.MapView;
+
+import java.util.ArrayList;
 
 
 public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener, MapView.POIItemEventListener {
@@ -30,6 +34,10 @@ public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLo
     MapView mapView;
     ViewGroup mapViewContainer;
     Button writeMapAddBtn;
+   // Button writeMapRemoveBtn;
+    boolean MarkerExists = false;
+    public static MapPoint mapPointNow;
+    ArrayList<MapPoint> PolyPoints = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,21 +70,16 @@ public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLo
         customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
 
         mapView.addPOIItem(customMarker); */
-
+        mapView.setPOIItemEventListener(this);
+        mapView.setMapViewEventListener(this);
         moveMyLocation(mapView);
 
-     /*   writeMapAddBtn = findViewById(R.id.writeMapAddBtn);
-        writeMapAddBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDtrDialog();
-            }
-        });   */
+
 
     }
 
     public void moveMyLocation(MapView mapView){
-        mapView.setMapViewEventListener(this);
+
         mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
 
         Handler mHandler = new Handler();
@@ -91,15 +94,9 @@ public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLo
     }
 
 
-     /*   mapView.setMapViewEventListener(this);
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-        if (!checkLocationServicesStatus()) {
-            showDialogForLocationServiceSetting();
-        }else {
-            checkRunTimePermission();
-        }   */
 
-  /*  public void showDtrDialog(String DtrName){
+    public void showDtrDialog(MapView mapView, MapPoint mapPoint){
+
         AlertDialog.Builder DtrDialog = new AlertDialog.Builder(this);
         DtrDialog.setTitle("장소 이름을 입력하세요");       // 제목 설정
         // EditText 삽입하기
@@ -112,20 +109,129 @@ public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLo
             public void onClick(DialogInterface dialog, int which) {
 
                 // Text 값 받아서 처리
-                String PWCheckEditText1 = PWCheckEditText.getText().toString();
+                String DtrName = DtrNameEditText.getText().toString();
                 dialog.dismiss();     //닫기
+                //addDtr();
+                // 도토리 추가
+                MapPOIItem customMarker2 = new MapPOIItem();
+                customMarker2.setItemName(DtrName); // 이게 필수로 들어가야하는데 => 말풍선 안보이게 가능할듯
+                customMarker2.setTag(6);
+                customMarker2.setMapPoint(mapPoint);
+                customMarker2.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+                customMarker2.setCustomImageResourceId(R.drawable.acorn2); // 마커 이미지.
+                customMarker2.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+                customMarker2.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+                customMarker2.setShowCalloutBalloonOnTouch(false);
 
-                if (PWCheckEditText1=="1234") {
-                    //Intent intent = new Intent(getApplicationContext(), ProfileEditPage.class);
-                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    //startActivity(intent);
-                } else {
-                    Toast.makeText(getApplicationContext(), "비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show();
-                }
+                mapView.addPOIItem(customMarker2);
+                // 이미 만들어진 콘텐츠id 에다가 집어넣음
+
+                PolyPoints.add(mapPoint);
+                drawPoly(mapView, PolyPoints);
+
             }
         });
+
+      /*  DtrDialog.setNegativeButton("삭제", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Text 값 받아서 처리
+                String DtrName = DtrNameEditText.getText().toString();
+                dialog.dismiss();     //닫기
+
+            }
+        }); */
+
+        DtrDialog.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Text 값 받아서 처리
+                dialog.dismiss();     //닫기
+
+            }
+        });
+
         DtrDialog.show();
-    } */
+    }
+
+
+/*    public void showDtrDialog1(MapView mapView, MapPOIItem mapPOIItem){
+
+        AlertDialog.Builder DtrDialog = new AlertDialog.Builder(this);
+        DtrDialog.setTitle("장소 이름을 입력하세요");       // 제목 설정
+        // EditText 삽입하기
+        final EditText DtrNameEditText2 = new EditText(this);
+        String ExistName = mapPOIItem.getItemName();
+        DtrNameEditText2.setText(ExistName);
+        DtrDialog.setView(DtrNameEditText2);
+
+        // 확인 버튼 설정
+        DtrDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                    // Text 값 받아서 처리
+                    String DtrName = DtrNameEditText2.getText().toString();
+                    dialog.dismiss();     //닫기
+                    //addDtr();
+                    // 도토리 추가
+                    MapPOIItem customMarker3 = new MapPOIItem();
+                    customMarker3.setItemName(DtrName); // 이게 필수로 들어가야하는데 => 말풍선 안보이게 가능할듯
+                    customMarker3.setTag(7);
+                    MapPoint mapPoint = mapPOIItem.getMapPoint();
+                    customMarker3.setMapPoint(mapPoint);
+                    customMarker3.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+                    customMarker3.setCustomImageResourceId(R.drawable.acorn2); // 마커 이미지.
+                    customMarker3.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+                    customMarker3.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+                    customMarker3.setShowCalloutBalloonOnTouch(false);
+
+                    mapView.addPOIItem(customMarker3);
+
+            }
+        });
+
+        DtrDialog.setNegativeButton("삭제", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Text 값 받아서 처리
+                String DtrName = DtrNameEditText.getText().toString();
+                dialog.dismiss();     //닫기
+
+            }
+        });
+
+        DtrDialog.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Text 값 받아서 처리
+                dialog.dismiss();     //닫기
+
+            }
+        });
+
+        DtrDialog.show();
+    }  */
+
+    public void drawPoly(MapView mapView, ArrayList<MapPoint> PolyPoints){
+
+        MapPolyline polyline = new MapPolyline();
+        polyline.setTag(1000);
+        polyline.setLineColor(Color.argb(128, 255, 51, 0)); // Polyline 컬러 지정.
+
+        // Polyline 좌표 지정.
+        for (int i=0; i < PolyPoints.size(); i++) {
+            MapPoint PolyPoint = PolyPoints.get(i);
+            polyline.addPoint(PolyPoint);
+        }
+
+// Polyline 지도에 올리기.
+        mapView.addPolyline(polyline);
+    }
 
     @Override
     protected void onDestroy() {
@@ -206,22 +312,42 @@ public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLo
         //marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
 
         mapView.addPOIItem(marker); */
-        mapView.removeAllPOIItems();
 
-        MapPOIItem customMarker = new MapPOIItem();
-        customMarker.setItemName("Custom Marker"); // 이게 필수로 들어가야하는데 => 말풍선 안보이게 가능할듯
-        customMarker.setTag(5);
-        customMarker.setMapPoint(mapPoint);
-        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
-        customMarker.setCustomImageResourceId(R.drawable.acorn2); // 마커 이미지.
-        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
-        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
-        customMarker.setShowCalloutBalloonOnTouch(false);
+        if(MarkerExists) {
+            MapPOIItem ExistItem = mapView.findPOIItemByTag(5);
+            mapView.removePOIItem(ExistItem);
+            MarkerExists=false;
+            writeMapAddBtn = findViewById(R.id.writeMapAddBtn);
+            writeMapAddBtn.setVisibility(View.INVISIBLE);
 
-        mapView.addPOIItem(customMarker);
+        } else {
+            MapPOIItem customMarker = new MapPOIItem();
+            customMarker.setItemName(""); // 이게 필수로 들어가야하는데 => 말풍선 안보이게 가능할듯
+            customMarker.setTag(5);
+            customMarker.setMapPoint(mapPoint);
+            customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+            customMarker.setCustomImageResourceId(R.drawable.acorn2); // 마커 이미지.
+            customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+            customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+            customMarker.setShowCalloutBalloonOnTouch(false);
+                mapView.addPOIItem(customMarker);
+
+                MarkerExists=true;
+            writeMapAddBtn = findViewById(R.id.writeMapAddBtn);
+            writeMapAddBtn.setVisibility(View.VISIBLE);
+
+
+        }
+
         writeMapAddBtn = findViewById(R.id.writeMapAddBtn);
-        writeMapAddBtn.setVisibility(View.VISIBLE);
-
+        writeMapAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDtrDialog(mapView, mapPoint);
+            }
+        });
+      //  writeMapRemoveBtn = findViewById(R.id.writeMapRemoveBtn);
+      //  writeMapRemoveBtn.setVisibility(View.INVISIBLE);
 
     }
 
@@ -253,7 +379,24 @@ public class WriteMapPage extends AppCompatActivity implements MapView.CurrentLo
     @Override
     public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
 
-        Toast.makeText(getApplicationContext(), "마커 선택", Toast.LENGTH_SHORT).show();
+
+      // showDtrDialog1(mapView, mapPOIItem);
+        String DtrName = mapPOIItem.getItemName();
+        Toast.makeText(getApplicationContext(), DtrName, Toast.LENGTH_SHORT).show();
+
+     /*  삭제버튼
+        writeMapAddBtn = findViewById(R.id.writeMapAddBtn);
+        writeMapAddBtn.setVisibility(View.INVISIBLE);
+        writeMapRemoveBtn = findViewById(R.id.writeMapRemoveBtn);
+        writeMapRemoveBtn.setVisibility(View.VISIBLE);
+        writeMapRemoveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapView.removePOIItem(mapPOIItem);
+            }
+        });  */
+
+
        // String DtrName = mapPOIItem.getItemName();
        // showDtrDialog(DtrName);
 
