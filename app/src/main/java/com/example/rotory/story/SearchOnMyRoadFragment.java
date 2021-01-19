1fc8ae78b66ae9a8d505f611b2d89dfe87a092ad
blanc0548 <kimbr0548@gmail.com>
2021-01-19 17:46:38 +0900

    이야기 주소 검색 창 수정 및 추가

---
+++ app/src/main/java/com/example/rotory/story/SearchOnMyRoadFragment.java
@@ -0,0 +1,50 @@
+package com.example.rotory.story;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rotory.R;
+
+
+public class SearchOnMyRoadFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public SearchOnMyRoadFragment() {
+        // Required empty public constructor
+    }
+
+    public static SearchOnMyRoadFragment newInstance(String param1, String param2) {
+        SearchOnMyRoadFragment fragment = new SearchOnMyRoadFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_on_my_road, container, false);
+    }
+}
